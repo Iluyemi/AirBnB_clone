c73e0f393be83252ecc92999fa1f9a5d434edc11
PEDA-ALPHA1 <daep100@gmail.com>
2022-11-28 07:05:25 -0800

    Pycodestyle compliance

--- console.py
+++ console.py
@@ -6,6 +6,7 @@
 import cmd
 import json
 
+
 class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
@@ -14,20 +15,22 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, args):
         print()
         return True
+
     def help_EOF(self):
-        print("EOF (End Of File): Command to terminate process and exit the program")
+        print("EOF (End Of File): Command to terminate process and\
+                exit the program")
 
 # quit command exits the program
     def do_quit(self, args):
         return True
+
     def help_quit(self):
         print("Quit: Command to exit the program. Similar to EOF")
 
 # Empty_line + enter would not execute
     def emptyline(self):
         pass
-    
+
 
 if __name__ == "__main__":
-        HBNBCommand().cmdloop()
-
+    HBNBCommand().cmdloop()
